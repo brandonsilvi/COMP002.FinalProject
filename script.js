62c18a3a5ed32af1be50a80089fe77b0bc26be65
Brandon Silvi <brandon.silvi@gmail.com>
2025-11-30 14:48:20 -0800

    added || to ensure scoreboard does not start with NaN

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ let board = ['', '', '', '', '', '', '', '', '']; //arrray for the 9 squares
 let currentPlayer = 'X'; //current player
 let gameOver = false; //game over flag
 
-let scoreX = Number(localStorage.getItem('scoreX'));  
-let scoreO = Number(localStorage.getItem('scoreO'));
+let scoreX = Number(localStorage.getItem('scoreX')) || 0;  // || if no score found, default to 0 and prevent NaN.
+let scoreO = Number(localStorage.getItem('scoreO')) || 0;
 //retrieve score for X & O from local storage and converts the stored string into a number
 
 const squares = document.querySelectorAll('.game-square'); //select all squares
