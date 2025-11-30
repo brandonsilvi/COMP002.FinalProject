793484d51ec196f1adbabfd7b5437f46fd8a6c45
Brandon Silvi <brandon.silvi@gmail.com>
2025-11-29 18:24:31 -0800

    variables set, local storage for scores set

--- script.js
+++ script.js
@@ -1,13 +1,36 @@
+//State variables
+
 let board = ['', '', '', '', '', '', '', '', '']; //arrray for the 9 squares
-
 let currentPlayer = 'X'; //current player
-
 let gameOver = false; //game over flag
 
-let scoreX = 0;
-let scoreO = 0; //scores for players
+let scoreX = Number(localStorage.getItem('scoreX')); 
+let scoreO = Number(localStorage.getItem('scoreO'));
+//retrieve score for X & O from local storage and converts the stored string into a number
+
+const squares = document.querySelectorAll('.square'); //select all squares
+const turnDisplay = document.getElementById('turnDisplay'); //display for current turn
+const btnPlayAgain = document.getElementById('btnPlayAgain'); //play again button
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 function updateScore() {
     document.getElementById('scoreX').innerText = scoreX;
     document.getElementById('scoreO').innerText = scoreO;
 }
+
